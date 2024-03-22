bab7341d2909bbbf5140b5cd7657a8242cc89eb7
Philip Cali <philip.cali@gmail.com>
2024-03-21 22:13:03 -0400

    fix: protocol

--- pinthesky/router.py
+++ pinthesky/router.py
@@ -41,7 +41,7 @@ def default(event, context):
     print(f'Event context {context}')
     management = boto3.client(
         'apigatewaymanagementapi',
-        endpoint_url=f'{event["requestContext"]["domainName"]}/{event["requestContext"]["stage"]}')
+        endpoint_url=f'https://{event["requestContext"]["domainName"]}/{event["requestContext"]["stage"]}')
     management.post_to_connection(
         ConnectionId=event["requestContext"]["connectionId"],
         Data=f'Use the sendMessage route to send a message.'.encode(encoding='utf-8'),
@@ -58,7 +58,7 @@ def send_message(event, context):
     account = os.getenv('ACCOUNT_ID')
     management = boto3.client(
         'apigatewaymanagementapi',
-        endpoint_url=f'{event["requestContext"]["domainName"]}/{event["requestContext"]["stage"]}')
+        endpoint_url=f'https://{event["requestContext"]["domainName"]}/{event["requestContext"]["stage"]}')
     resp = table.query(KeyConditionExpression=Key("PK").eq(f'DataConnections:{account}'))
     payload = json.loads(event['body'])
     for item in resp["Items"]:
